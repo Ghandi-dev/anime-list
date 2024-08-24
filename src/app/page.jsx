7c9cb154c2fbab72ba663bb443d7cb99f58b15a0
Ghandi-dev <scoutghandi31@gmail.com>
2024-08-24 10:31:55 +0700

    feat: add last and first page pagination

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
+import { getAnimeResponse } from "./libs/api-libs";
 
 const Page = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`);
-  const topAnime = await res.json();
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`);
+  // const topAnime = await res.json();
+  const topAnime = await getAnimeResponse("/top/anime", "limit=8");
 
   return (
     <>
--- src/components/Utilities/Pagination.jsx
+++ src/components/Utilities/Pagination.jsx
@@ -1,4 +1,4 @@
-function Pagination({ page, lastPage, setPage }) {
+function Pagination({ page, lastPage = 1, setPage }) {
   const isBrowser = () => typeof window !== "undefined"; //The approach recommended by Next.js
 
   const scrollToTop = () => {
@@ -15,18 +15,42 @@ function Pagination({ page, lastPage, setPage }) {
     setPage(page - 1);
     scrollToTop();
   };
+  const handlerLastPage = () => {
+    setPage(lastPage);
+    scrollToTop();
+  };
+
+  const handlerFirstPage = () => {
+    setPage(1);
+    scrollToTop();
+  };
+
   if (lastPage === 0) {
     return null;
   }
   return (
-    <div className="flex items-center justify-center py-4 px-2 gap-8 text-color-primary text-2xl">
-      <button className="hover:text-color-accent transition-all" onClick={() => handlerPreviousPage()} disabled={page === 1}>
-        &laquo;
-      </button>
+    <div className="flex items-center justify-center py-4 px-2 md:gap-8 gap-4 text-color-primary md:text-2xl text-sm">
+      {page <= 1 ? null : (
+        <div className="flex md:gap-8 gap-4">
+          <button className="hover:text-color-accent transition-all" onClick={() => handlerFirstPage()}>
+            First
+          </button>
+          <button className="hover:text-color-accent transition-all" onClick={() => handlerPreviousPage()}>
+            &laquo;
+          </button>
+        </div>
+      )}
       <p>{`${page} of ${lastPage}`}</p>
-      <button className="hover:text-color-accent transition-all" onClick={() => handlerNextPage()} disabled={page === lastPage}>
-        &raquo;
-      </button>
+      {page === lastPage ? null : (
+        <div className="flex md:gap-8 gap-4">
+          <button className="hover:text-color-accent transition-all" onClick={() => handlerNextPage()}>
+            &raquo;
+          </button>
+          <button className="hover:text-color-accent transition-all" onClick={() => handlerLastPage()}>
+            Last
+          </button>
+        </div>
+      )}
     </div>
   );
 }
